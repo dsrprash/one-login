e8e9b39335fea8abe1ca60eeee3d975dc26d545c
Prashanth <dsr.prashanth95@gmail.com>
2024-03-25 11:28:51 +0530

    shifted org details to externl json file

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,29 @@
 let orgs = new Map();
-orgs.set('dev', {name:'Salesforce Dev Edition', url:'https://prash-env-dev-ed.lightning.force.com/', desc: 'Ths is personal developer edition org'});
-orgs.set('cpqdev',{name:'CPQ Dev', url:'https://prash-cpq-dev-ed.my.salesforce.com/', desc:'This CPQ pre-installed org'});
-orgs.set('playground', {name: 'Salesforce Playground org', url: 'https://cunning-moose-38507-dev-ed.my.salesforce.com/', desc: 'This is playground org'});
+// orgs.set('dev', {name:'Salesforce Dev Edition', url:'https://prash-env-dev-ed.lightning.force.com/', desc: 'Ths is personal developer edition org'});
+// orgs.set('cpqdev',{name:'CPQ Dev', url:'https://prash-cpq-dev-ed.my.salesforce.com/', desc:'This CPQ pre-installed org'});
+// orgs.set('playground', {name: 'Salesforce Playground org', url: 'https://cunning-moose-38507-dev-ed.my.salesforce.com/', desc: 'This is playground org'});
 
-function loadFunction(){
+async function loadFunction(){
     document.getElementById('date').innerText = new Date().toDateString();
     let cardsDiv = document.getElementById('cards');
     
-
-    let cardsHtml = '';
-    for([key, value] of orgs){
-        cardsHtml = cardsHtml + 
-                    '<div class="card" onclick="handleClick(\''+key+'\');">'+
-                        '<h2>'+value.name+'</h2>' +
-                        '<p class="card-description">'+value.desc+'</p>'+
-                    '</div>';
-    }
-    cardsDiv.innerHTML = cardsHtml;
+    let orgdetails = await fetch('scripts/data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json); 
+        orgs = new Map(Object.entries(json));
+    })
+    .then( (res) => {
+        let cardsHtml = '';
+        for([key, value] of orgs){
+            cardsHtml = cardsHtml + 
+                        '<div class="card" onclick="handleClick(\''+key+'\');">'+
+                            '<h2>'+value.name+'</h2>' +
+                            '<p class="card-description">'+value.desc+'</p>'+
+                        '</div>';
+        }
+        cardsDiv.innerHTML = cardsHtml;
+    })
 }
 
 
